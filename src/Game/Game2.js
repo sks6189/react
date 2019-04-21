9bbc0e27d39f2dc8e3a13e79ad4e99994d81df58
이민종 <sks6189@gmail.com>
2019-04-22 01:59:23 +0900

    test

--- src/Game/Game2.js
+++ src/Game/Game2.js
@@ -13,13 +13,24 @@ class Button extends Component{
 }
 
 class Game2 extends Component {
+    updateValue(){
+        let num = Math.round(Math.random()*100);
+        console.log(this.props);
+        console.log(num);
+    }
+
+    constructor(props){
+        super(props);
+        //this.number =
+    }
 
     render(){
         let setTitle = "GAME";
         return(
             <div className="game">
                 <div className="game-board">
-                    <Button/>
+                    <p>1</p>
+                    <button onclick={this.updateValue}>클릭</button>
                 </div>
             </div>
         )
